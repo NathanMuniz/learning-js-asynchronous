2c801f3cd2d98df55023b5856dd7dbb3b9a79e0e
Qubitsys <qubitsysx@gmail.com>
2022-10-18 22:04:17 -0300

    mdn-prommises

--- promises/mdn-promises-examples.js
+++ promises/mdn-promises-examples.js
@@ -1,3 +1,4 @@
+/*
 fecthPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json')
 
 console.log(fetchPromise); // Promise { <state>: "pending" }
@@ -7,3 +8,47 @@ fetchPromise.then((response) => {
 });
 
 console.log("Started request...");
+
+*/
+
+
+
+/*
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetchPromise.then((response) => {
+  const jsonPromise = response.json();
+  jsonPromise.then((data) => {
+    console.log(data[0].name)
+  });
+});
+*/
+
+
+/* rewriting - when you has alot promises */
+
+/*
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetchPromise
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data[0].name);
+  });
+
+*/
+
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+
+fetchPromise
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data[0].name);
+  });
+
